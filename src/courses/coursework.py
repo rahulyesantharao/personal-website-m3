46e13142ede7f1a3a00989d5c7cad1a347cf14fe
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2020-08-18 00:15:06 -0500

    update coursework with project links, todo: actually add more projects

--- src/courses/coursework.py
+++ src/courses/coursework.py
@@ -19,7 +19,10 @@ def func():
         for (semester, classes) in data:
             ret += f"<h3>{get_semester_emoji(semester)} {semester}</h3>"
             ret += "<ul>"
-            for (class_number, class_name) in classes:
-                ret += f'<li><a href="http://student.mit.edu/catalog/search.cgi?search={class_number}">{class_number}</a> - {class_name}</li>'
+            for (class_number, class_name, class_proj_link) in classes:
+                ret += f'<li><a href="http://student.mit.edu/catalog/search.cgi?search={class_number}">{class_number}</a> - {class_name}'
+                if class_proj_link:
+                    ret += f" (<a href={class_proj_link}>project</a>)"
+                ret += "</li>"
             ret += "</ul>"
     return ret
