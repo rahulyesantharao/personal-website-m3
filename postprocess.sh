6062a79b5f4687a87fe3b002edcb8672b06a3262
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2022-09-09 11:54:34 -0400

    update postprocess.sh with debug output

--- postprocess.sh
+++ postprocess.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 cssfile=$1
 jsfile=$2
+echo "start postcss"
 npx postcss "$cssfile" -d build/css/
+echo "start uglifyjs"
 npx uglifyjs --compress --mangle -o "$jsfile" -- "$jsfile"
+echo "start html-minifier"
 npx html-minifier -c html-config.conf --input-dir build/ --output-dir build/ --file-ext html
